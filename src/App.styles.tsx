902b9c8ead958991691af4f5ccede766c546d374
Niamh McCooey <niamhmccooey@gmail.com>
2024-03-05 20:50:05 +0000

    feat: location buttons

--- src/App.styles.tsx
+++ src/App.styles.tsx
@@ -1,7 +1,7 @@
-import styled from 'styled-components';
+import styled, { keyframes, css } from 'styled-components';
 
 export const Wrap = styled.div`
-	svg {
+	.wave {
 		transform: translateY(100%); /* start position below viewport */
 		position: absolute;
 		top: 0;
@@ -18,4 +18,84 @@ export const MidWave = styled.svg`
 
 export const BottomWave = styled.svg`
 	z-index: 1;
+`;
+
+const fadeIn = keyframes`
+  from {
+    opacity: 0;
+  }
+  to {
+    opacity: 1;
+  }
+`;
+
+export const UiWrap = styled.div`
+	position: relative;
+	text-align: center;
+	min-height: 100vh;
+	z-index: 99;
+	display: flex;
+	flex-flow: column nowrap;
+	align-items: center;
+	justify-content: center;
+	opacity: 0;
+	animation: ${fadeIn} 1s ease-out 2s forwards;
+`;
+
+export const FlexWrap = styled.div`
+	display: flex;
+	margin: 2rem 0;
+	align-items: center;
+	flex-flow: column nowrap;
+`;
+
+export const Button = styled.button<{ kind?: string; dropdownOpen?: boolean }>`
+	background: ${({ dropdownOpen }) => (dropdownOpen ? 'rgba(255, 255, 255, 0.05)' : 'none')};
+	color: #fff;
+	padding: 2rem 0;
+	font-size: 1rem;
+	font-weight: 100;
+	text-align: center;
+	border: solid 1px #fff;
+	border-radius: 1.25rem;
+	outline: none;
+	position: relative;
+	padding: 1rem 2rem;
+	cursor: pointer;
+	width: 10rem;
+	margin: 1rem 0;
+
+	${({ kind }) =>
+		kind === 'secondary' &&
+		css`
+			border: none;
+
+			svg {
+				margin: 0 0.75rem;
+			}
+			&:hover {
+				background: rgba(255, 255, 255, 0.05);
+			}
+		`};
+`;
+
+export const Dropdown = styled.ul<{ dropdownOpen: boolean }>`
+	opacity: ${({ dropdownOpen }) => (dropdownOpen ? '1' : '0')};
+	background: rgba(255, 255, 255, 0.05);
+	border-radius: 1.25rem;
+	padding: 0;
+	list-style: none;
+	overflow: hidden;
+	margin: 0;
+`;
+
+export const DropdownItem = styled.li`
+	padding: 0;
+
+	button {
+		margin: 0;
+		&:hover {
+			background: none;
+		}
+	}
 `;
--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,24 @@
 import gsap from 'gsap';
+import { useState } from 'react';
 import { useGSAP } from '@gsap/react';
-import { Wrap, TopWave, MidWave, BottomWave } from './App.styles';
+import Waves from './components/Waves';
+import DropdownMenu from './components/DropdownMenu';
+import { Wrap, UiWrap, FlexWrap, Button } from './App.styles';
+
+const locations: { name: string }[] = [
+	{ name: 'Dollymount' },
+	{ name: 'Half Moon' },
+	{ name: 'Malahide' },
+	{ name: 'Portmarnock' },
+	{ name: 'Rush' },
+	{ name: 'Skerries' },
+];
 
 const bob = (
 	selector: string,
 	initialY: string,
 	swipeInDuration: number,
+	swipeInDelay: number,
 	finalY: string,
 	bobDuration: number,
 	bobDelay: number
@@ -13,6 +26,7 @@ const bob = (
 	gsap.to(selector, {
 		duration: swipeInDuration,
 		y: initialY,
+		delay: swipeInDelay,
 		ease: 'elastic.out(0.5, 0.75)',
 		onComplete: () => {
 			gsap.to(selector, {
@@ -28,50 +42,49 @@ const bob = (
 };
 
 const App = () => {
+	const [moreOpen, setMoreOpen] = useState<boolean>(false);
+
 	useGSAP(() => {
-		bob('.bottom-wave', '-50%', 4, '-49.75%', 5, 0);
-		bob('.mid-wave', '-45%', 4.5, '-45.6%', 6, 0.95);
-		bob('.top-wave', '-2%', 5, '-1.25%', 5, 1.6);
+		bob('.bottom-wave', '-50%', 4, 0.3, '-49.75%', 5, 0);
+		bob('.mid-wave', '-45%', 4.5, 0.4, '-45.6%', 6, 0.95);
+		bob('.top-wave', '-2%', 5, 0.5, '-1.25%', 5, 1.6);
 	}, {});
 
 	return (
 		<Wrap>
-			<TopWave
-				fill='none'
-				width='150%'
-				height='180%'
-				viewBox='0 0 400 892'
-				className='top-wave'
-				preserveAspectRatio='none'
-				xmlns='http://www.w3.org/2000/svg'
-			>
-				<path
-					d='M-147 139.5C186.802 -173.693 824 374 1395.5 51.5C1967 -271 1526 1047.5 1526 1047.5L-381.5 1069C-381.5 1069 -480.802 452.693 -147 139.5Z'
-					fill='#130E55'
-				/>
-			</TopWave>
-			<MidWave
-				width='100%'
-				height='180%'
-				viewBox='0 0 400 200'
-				fill='none'
-				className='mid-wave'
-				xmlns='http://www.w3.org/2000/svg'
-			>
-				<path
-					fill='#461d61'
-					d='M-36.3726 60.7338C133.789 77.5408 211 95.7339 419.245 5.0795C627.491 -85.5749 405 1067.23 405 1067.23H-36.3726C-36.3726 1067.23 -206.534 43.9267 -36.3726 60.7338Z'
-				/>
-			</MidWave>
-			<BottomWave
-				width='100%'
-				height='180%'
-				viewBox='0 0 300 100'
-				className='bottom-wave'
-				xmlns='http://www.w3.org/2000/svg'
-			>
-				<path fill='#8d75eb' d='M-1 100C21.5985 10 308.993 100 393 100V1033H-1V100Z' />
-			</BottomWave>
+			<Waves />
+			<UiWrap>
+				<h1>Where to?</h1>
+				<FlexWrap>
+					<Button>40 Foot</Button>
+					<Button>Vico Baths</Button>
+					<Button>Seapoint</Button>
+					<Button>Killiney</Button>
+					<Button kind='secondary' dropdownOpen={moreOpen} onClick={() => setMoreOpen(!moreOpen)}>
+						<span>More</span>
+						<svg
+							width='13'
+							height='8'
+							viewBox='0 0 13 8'
+							fill='none'
+							xmlns='http://www.w3.org/2000/svg'
+						>
+							{!moreOpen ? (
+								<path
+									d='M11.2401 0.294922L6.65015 4.87492L2.06015 0.294922L0.650146 1.70492L6.65015 7.70492L12.6501 1.70492L11.2401 0.294922Z'
+									fill='white'
+								/>
+							) : (
+								<path
+									d='M1.43958 7.71806L6.01646 3.12494L10.6195 7.69178L12.0255 6.27775L6.00835 0.29495L0.0255542 6.3121L1.43958 7.71806Z'
+									fill='white'
+								/>
+							)}
+						</svg>
+					</Button>
+					<DropdownMenu locations={locations} dropdownOpen={moreOpen} />
+				</FlexWrap>
+			</UiWrap>
 		</Wrap>
 	);
 };
